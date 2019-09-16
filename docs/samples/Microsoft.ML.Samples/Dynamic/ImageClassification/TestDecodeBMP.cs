4d678e1ca6a6c1bbafda8e21612d0f58ff1e9505
harshithapv <54084812+harshithapv@users.noreply.github.com>
2019-09-16 14:50:27 -0700

    Changed ImageClassification API to read Bitmap images. Adding the new files

---
+++ docs/samples/Microsoft.ML.Samples/Dynamic/ImageClassification/TestDecodeBMP.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.Transforms;
+using static Microsoft.ML.DataOperationsCatalog;
+using System.Linq;
+using Microsoft.ML.Data;
+using System.IO.Compression;
+using System.Threading;
+using System.Net;
+using Microsoft.ML.Transforms.Image;
+using System.Drawing;
+
+namespace Samples.Dynamic
+{
+    public class TestDecodeBMP
+    {
+        public static void Example()
+        {
+            string ImageFilePath = "C:\\Users\\havenka\\Desktop\\test\\tomato.jpg";
+
+            try
+            {
+                byte[] b = File.ReadAllBytes(ImageFilePath);
+
+                
+                
+                var bmp = new Bitmap(ImageFilePath);
+                //var b2 = bmp.LockBits(...);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey();
+        }
+        public static Bitmap ToImage(byte[] arr)
+        {
+
+            Bitmap bmp;
+            using (var ms = new MemoryStream(arr))
+            {
+                bmp = new Bitmap(ms);
+            }
+            return bmp;
+
+        }
+    }
+}
+
+    
+
+
